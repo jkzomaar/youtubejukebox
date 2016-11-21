733220cb81369d470c4c504c82a52893e8170590
Bart Willems <ps3bartwillems@gmail.com>
2016-11-21 23:32:58 +0100

    Fixed wallpaper on mobile view, fixed pages

--- client/js/main.js
+++ client/js/main.js
@@ -9,23 +9,12 @@ $( document ).ready(function() {
       $('#search-button').prop('disabled', false);
   });
 
+  $('#jukeBox').click(function(){
+      $('#search-result').html('');
+  });
+
 });
 
-//make enter in the search box click the search button
-// document.getElementById("query")
-//     .addEventListener("keyup", function(event) {
-//     event.preventDefault();
-//     if (event.keyCode == 13) {
-//         document.getElementById("search-button").click();
-//     }
-// });
-
-//enable search button when typing new query (to avoid spamming)
-// document.getElementById("query")
-//     .addEventListener("keypress", function() {
-//         document.getElementById("search-button").disabled = false;
-// });
-
 function showQ(){
- $('#search-container').html('<pre class="block-center">todo: implement queue viewer</pre>');
+ $('#search-result').html('<pre class="block-center">todo: implement queue viewer</pre>');
 }
