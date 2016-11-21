587570e784d763f959494f7600d00e7d6eae9d74
Kenny <vdmkenny@inuits.eu>
2016-11-21 10:43:39 +0000

    initial local css, improved scaling but still kind of wonky
    Signed-off-by: Kenny <vdmkenny@inuits.eu>

--- client/js/search.js
+++ client/js/search.js
@@ -19,11 +19,13 @@ function search() {
     //$('#search-container').html('<pre>' + str + '</pre>');
     var json = response.result;
     $('#search-container').html('</br></br></br>');
+    $('#search-container').append('<div class="row">');
     for(var i = 0; i < json.items.length; i++) {
     var obj = json.items[i];
     console.log(obj.snippet.title);
-    $('#search-container').append('<div class="row"><div class="col-sm-0 col-md-4"><div class="thumbnail"><img src="' + obj.snippet.thumbnails.high.url + '" alt="thumbnail"><div class="caption"><h3>' + obj.snippet.title + '</h3><p>' + obj.snippet.description + '</p><p><a href="#" class="btn btn-primary" role="button">Button</a></div></div></div></div>');
+    $('#search-container').append('<div class="searchresult col-sm-1 col-md-4"><div class="thumbnail"><img src="' + obj.snippet.thumbnails.high.url + '" alt="thumbnail"><div class="caption"><h3 class="vtitle">' + obj.snippet.title + '</h3><p class="description">' + obj.snippet.description + '</p><p><a href="#" class="btn btn-primary" role="button">Put in Queue</a></div></div></div>');
 } 
+    $('#search-container').append('</div>');
   });
 }
 
