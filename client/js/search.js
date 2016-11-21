73c64cd882fe104453f7646da7a39fab40211cc3
Kenny <vdmkenny@inuits.eu>
2016-11-21 14:13:47 +0000

    tiny gui tweaks
    Signed-off-by: Kenny <vdmkenny@inuits.eu>

--- client/js/search.js
+++ client/js/search.js
@@ -5,7 +5,7 @@ function handleAPILoaded() {
 
 // Search for a specified string.
 function search() {
-  var q = $('#query').val();
+  var q = $('#query').val() + " song";
   var request = gapi.client.youtube.search.list({
     q: q,
     part: 'snippet',
