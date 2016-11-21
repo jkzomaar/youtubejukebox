ca6bf118f730300b98537357189d150fb31c1f09
Kenny <vdmkenny@inuits.eu>
2016-11-21 09:50:18 +0000

    basic searching - now with html objects
    Signed-off-by: Kenny <vdmkenny@inuits.eu>

--- client/js/search.js
+++ client/js/search.js
@@ -15,8 +15,15 @@ function search() {
   });
 
   request.execute(function(response) {
-    var str = JSON.stringify(response.result);
-    $('#search-container').html('<pre>' + str + '</pre>');
+    //var str = JSON.stringify(response.result);
+    //$('#search-container').html('<pre>' + str + '</pre>');
+    var json = response.result;
+    $('#search-container').html('</br></br></br>');
+    for(var i = 0; i < json.items.length; i++) {
+    var obj = json.items[i];
+    console.log(obj.snippet.title);
+    $('#search-container').append('<div class="row"><div class="col-sm-0 col-md-4"><div class="thumbnail"><img src="' + obj.snippet.thumbnails.high.url + '" alt="thumbnail"><div class="caption"><h3>' + obj.snippet.title + '</h3><p>' + obj.snippet.description + '</p><p><a href="#" class="btn btn-primary" role="button">Button</a></div></div></div></div>');
+} 
   });
 }
 
