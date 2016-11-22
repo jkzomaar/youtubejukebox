9475b78e1d39d7fb1132040ace895647bb34881d
Kenny <vdmkenny@inuits.eu>
2016-11-22 16:11:49 +0000

    break layout but implement video duration call
    Signed-off-by: Kenny <vdmkenny@inuits.eu>

--- client/js/main.js
+++ client/js/main.js
@@ -28,3 +28,71 @@ $( document ).ready(function() {
 function showQ(){
  $('#search-result').html('<pre class="block-center">todo: implement queue viewer</pre>');
 }
+
+//http://stackoverflow.com/a/24290476/2820952
+function convertDuration(t){ 
+		//dividing period from time
+		var	x = t.split('T'),
+			duration = '',
+			time = {},
+			period = {},
+			//just shortcuts
+			s = 'string',
+			v = 'variables',
+			l = 'letters',
+			// store the information about ISO8601 duration format and the divided strings
+			d = {
+				period: {
+					string: x[0].substring(1,x[0].length),
+					len: 4,
+					// years, months, weeks, days
+					letters: ['Y', 'M', 'W', 'D'],
+					variables: {}
+				},
+				time: {
+					string: x[1],
+					len: 3,
+					// hours, minutes, seconds
+					letters: ['H', 'M', 'S'],
+					variables: {}
+				}
+			};
+		//in case the duration is a multiple of one day
+		if (!d.time.string) {
+			d.time.string = '';
+		}
+
+		for (var i in d) {
+			var len = d[i].len;
+			for (var j = 0; j < len; j++) {
+				d[i][s] = d[i][s].split(d[i][l][j]);
+				if (d[i][s].length>1) {
+					d[i][v][d[i][l][j]] = parseInt(d[i][s][0], 10);
+					d[i][s] = d[i][s][1];
+				} else {
+					d[i][v][d[i][l][j]] = 0;
+					d[i][s] = d[i][s][0];
+				}
+			}
+		} 
+		period = d.period.variables;
+		time = d.time.variables;
+		time.H += 	24 * period.D + 
+								24 * 7 * period.W +
+								24 * 7 * 4 * period.M + 
+								24 * 7 * 4 * 12 * period.Y;
+		
+		if (time.H) {
+			duration = time.H + ':';
+			if (time.M < 10) {
+				time.M = '0' + time.M;
+			}
+		}
+
+		if (time.S < 10) {
+			time.S = '0' + time.S;
+		}
+
+		duration += time.M + ':' + time.S;
+	return duration;
+}
--- client/js/search.js
+++ client/js/search.js
@@ -13,41 +13,66 @@ function handleAPILoaded() {
 
 // Search for a specified string.
 function search() {
-  window.scrollTo(0,0);
-  document.getElementById("search-button").disabled = true;
-  var q = $('#query').val() + " song";
-  var request = gapi.client.youtube.search.list({
-    q: q,
-    part: 'snippet',
-    maxResults: 20,
-    order: 'relevance',
-    type: 'video'
-  });
+    $('#search-result').html('');
+    var json = "";
+    var numberOfVideos = 20;
+    var currentVideo = 0;
+    window.scrollTo(0, 0);
+    document.getElementById("search-button").disabled = true;
+    var q = $('#query').val() + " song";
+    var request = gapi.client.youtube.search.list({
+        q: q,
+        part: 'snippet',
+        maxResults: 20,
+        order: 'relevance',
+        type: 'video'
+    });
 
-  request.execute(function(response) {
-    document.activeElement.blur();
-    var json = response.result;
-    var html = '';
-    // it is more performant to calculate the length once
-    var arrayLength = json.items.length;
+    request.execute(function(response) {
+        document.activeElement.blur();
+        json = response.result;
+        var arrayLength = json.items.length;
+        var html = "";
+        html += '<div class="grid" onload="window.dispatchEvent(new Event(\'resize\'));">';
+        $('#search-result').append(html);
+        for (var i = 0; i < arrayLength; i++) {
+            var index = i;
+            //recursive query to get the video duration
+            var requestDetail = gapi.client.youtube.videos.list({
+                part: 'contentDetails',
+                id: json.items[i].id.videoId
+            });
 
-    html += '<div class="grid" onload="window.dispatchEvent(new Event(\'resize\'));">';
-    for(var i = 0; i < arrayLength; i++){
-      html += '<div class="grid-item"><div class="searchresult thumbnail">';
-      html += '<img src="' + json.items[i].snippet.thumbnails.high.url  + '" alt="thumbnail" onload="window.dispatchEvent(new Event(\'resize\'));">';
-      html += '<div class="caption"><h3 class="vtitle">' + json.items[i].snippet.title + '</h3>';
-      html += '<p class="description">' + json.items[i].snippet.description + '</p></div>';
-      html += '<div class="queueBtn"><a href="#" class="btn btn-warning btn-lg center-block qbutton" role="button">Put in Queue</a></div></div></div>';
-    }
-    html += '</div>';
-    $('#search-result').html(html);
-  $('.grid').masonry({
-    // options...
-    itemSelector: '.grid-item',
-    columnWidth: 300,
-    fitWidth: true,
-    gutter: 10
-  });
-  new Promise( function() {setTimeout(function(){ window.dispatchEvent(new Event('resize'));}, 1000);} );  
-  });
+            requestDetail.execute(function(response) {
+                currentVideo += 1;
+                var tempVideo=(currentVideo-1);
+		var jsonDetail = response.result;
+                var duration=convertDuration(jsonDetail.items[0].contentDetails.duration);
+                var html = "";
+                html += '<div class="grid-item"><div class="searchresult thumbnail">';
+                html += '<img src="' + json.items[tempVideo].snippet.thumbnails.high.url + '" alt="thumbnail" onload="window.dispatchEvent(new Event(\'resize\'));">';
+                html += '<div class="caption"><h3 class="vtitle">' + json.items[tempVideo].snippet.title + ' (' + duration + ')</h3>';
+                html += '<p class="description">' + json.items[tempVideo].snippet.description + '</p></div>';
+                html += '<div class="queueBtn"><a href="#" class="btn btn-warning btn-lg center-block qbutton" role="button">Put in Queue</a></div></div></div>';
+                $('#search-result').append(html);
+                if (currentVideo == (numberOfVideos - 1)) {
+                    $('#search-result').append("</div>");
+                    $('.grid').masonry({
+                        // options...
+                        itemSelector: '.grid-item',
+                        columnWidth: 300,
+                        fitWidth: true,
+                        gutter: 10
+                    });
+                    new Promise(function() {
+                        setTimeout(function() {
+                            window.dispatchEvent(new Event('resize'));
+                        }, 1000);
+                    });
+                }
+            });
+        }
+
+    });
+
 }
