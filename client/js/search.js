48139d8114ad426ed15a9f70486dfe3444e85945
Kenny <vdmkenny@inuits.eu>
2016-11-22 16:40:49 +0000

    unborked some stuff
    Signed-off-by: Kenny <vdmkenny@inuits.eu>

--- client/js/search.js
+++ client/js/search.js
@@ -34,7 +34,6 @@ function search() {
         var arrayLength = json.items.length;
         var html = "";
         html += '<div class="grid" onload="window.dispatchEvent(new Event(\'resize\'));">';
-        $('#search-result').append(html);
         for (var i = 0; i < arrayLength; i++) {
             var index = i;
             //recursive query to get the video duration
@@ -48,15 +47,14 @@ function search() {
                 var tempVideo=(currentVideo-1);
 		var jsonDetail = response.result;
                 var duration=convertDuration(jsonDetail.items[0].contentDetails.duration);
-                var html = "";
                 html += '<div class="grid-item"><div class="searchresult thumbnail">';
                 html += '<img src="' + json.items[tempVideo].snippet.thumbnails.high.url + '" alt="thumbnail" onload="window.dispatchEvent(new Event(\'resize\'));">';
                 html += '<div class="caption"><h3 class="vtitle">' + json.items[tempVideo].snippet.title + ' (' + duration + ')</h3>';
                 html += '<p class="description">' + json.items[tempVideo].snippet.description + '</p></div>';
                 html += '<div class="queueBtn"><a href="#" class="btn btn-warning btn-lg center-block qbutton" role="button">Put in Queue</a></div></div></div>';
-                $('#search-result').append(html);
                 if (currentVideo == (numberOfVideos - 1)) {
-                    $('#search-result').append("</div>");
+                    html+="</div>";
+                    $('#search-result').append(html);
                     $('.grid').masonry({
                         // options...
                         itemSelector: '.grid-item',
