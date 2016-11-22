66364c58f34edfe489dd41ef49e0059dd20a2769
Kenny <vdmkenny@inuits.eu>
2016-11-22 06:56:05 +0000

    include masonry library
    Signed-off-by: Kenny <vdmkenny@inuits.eu>

--- client/js/main.js
+++ client/js/main.js
@@ -13,7 +13,16 @@ $( document ).ready(function() {
       $('#search-result').html('');
   });
 
+  $('.grid').masonry({
+    // options...
+    itemSelector: '.grid-item',
+    columnWidth: 200
+  });
+
+
 });
+
+
 
 function showQ(){
  $('#search-result').html('<pre class="block-center">todo: implement queue viewer</pre>');
