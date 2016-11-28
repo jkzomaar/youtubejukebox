4aaae92d689ee7ec451fcd0227958d9d41632365
Kenny <vdmkenny@inuits.eu>
2016-11-28 11:57:43 +0000

    added initial queueing script
    Signed-off-by: Kenny <vdmkenny@inuits.eu>

---
+++ server/scripts/download.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+#get local directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+VIDEO_DIR="$DIR/../video/"
+QUEUE_FILE="$DIR/../queue.csv"
+LOG_DIR="$DIR/../log"
+BLACKLIST_FILE=$DIR/../blacklist
+
+#get params
+VIDEO_ID=$1
+VIDEO_TITLE=$(youtube-dl --id https://www.youtube.com/watch?v=$VIDEO_ID -e --restrict-filenames --no-warnings)
+VIDEO_DURATION=$2
+
+if grep -q "$VIDEO_ID" "$BLACKLIST_FILE";then
+	echo "BLACKLISTED"
+	exit 1
+fi
+
+
+#get queue date
+FULLDATE=`date +%Y-%m-%d\ %H:%M:%S`
+DATE=`date +%Y-%m-%d`
+
+#create directory structure if not existing.
+touch $QUEUE_FILE
+mkdir -p $LOG_DIR
+LOG_FILE=$LOG_DIR/$DATE.log
+touch $LOG_FILE
+mkdir -p $VIDEO_DIR
+touch $BLACKLIST_FILE
+
+#go to videofolder
+cd $VIDEO_DIR
+
+#download file (youtube-dl skips duplicate downloads)
+youtube-dl -f 43 --id https://www.youtube.com/watch?v=$VIDEO_ID --quiet --restrict-filenames --no-warnings
+
+#create csv line
+VIDEOSTRING="$VIDEO_ID,\"$VIDEO_TITLE\",$VIDEO_DURATION,$FULLDATE"
+
+if grep -q "$VIDEO_ID" "$QUEUE_FILE"; then
+	echo "ALREADY_SCHEDULED"
+  	exit 1
+else
+	echo "Adding $VIDEOSTRING to play queue." >> $LOG_FILE
+	echo $VIDEOSTRING >> $QUEUE_FILE
+fi
+
+echo "SUCCESS"
+exit
+
