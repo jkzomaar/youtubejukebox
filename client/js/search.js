ed8f7c5e9e04d3674b9af514c55c5dd94afb1ed5
Administrator <admin@example.com>
2016-11-29 14:49:39 +0100

    Put modal in html, fixed all grid-item positions(I hope)

--- client/js/search.js
+++ client/js/search.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
     $('#search-button').click(function(){
-      search();
+        search();
     });
 
 });
@@ -8,7 +8,7 @@ $( document ).ready(function() {
 
 // After the API loads, call a function to enable the search box.
 function handleAPILoaded() {
-  $('#search-button').attr('disabled', false);
+    $('#search-button').attr('disabled', false);
 }
 
 // Search for a specified string.
@@ -26,8 +26,8 @@ function search() {
         maxResults: 20,
         order: 'relevance',
         type: 'video',
-	videoCategoryId: '10',
-	videoDuration: 'medium'
+        videoCategoryId: '10',
+        videoDuration: 'medium'
     });
 
     request.execute(function(response) {
@@ -47,44 +47,30 @@ function search() {
             requestDetail.execute(function(response) {
                 currentVideo += 1;
                 var tempVideo=(currentVideo-1);
-		var jsonDetail = response.result;
+                var jsonDetail = response.result;
                 var duration=convertDuration(jsonDetail.items[0].contentDetails.duration);
                 html += '<div class="grid-item"><div class="searchresult thumbnail">';
-                html += '<img src="' + json.items[tempVideo].snippet.thumbnails.high.url + '" alt="thumbnail" onload="window.dispatchEvent(new Event(\'resize\'));">';
+                html += '<img class="ytTumbnail" src="' + json.items[tempVideo].snippet.thumbnails.high.url + '" alt="thumbnail" onload="window.dispatchEvent(new Event(\'resize\'));">';
                 html += '<div class="caption"><h3 class="vtitle">' + json.items[tempVideo].snippet.title + ' (' + duration + ')</h3>';
                 html += '<p class="description">' + json.items[tempVideo].snippet.description + '</p></div>';
                 html += '<div class="queueBtn"><a href="#" class="btn btn-warning btn-lg center-block qbutton" onclick="requestVideo(\'' +json.items[tempVideo].id.videoId+'\', \'' + jsonDetail.items[0].contentDetails.duration + '\');">Put in Queue</a></div></div></div>';
                 if (currentVideo == (numberOfVideos - 1)) {
                     html+="</div>";
-		    html+="<div id='myModal' class='modal fade' role='dialog' style='margin-top: 75px;'>";
-		    html+="<div class='modal-dialog'>";
-	            html+="<div class='modal-content'>";
-	            html+="<div class='modal-header'>";
-		    html+="<button type='button' class='close' data-dismiss='modal'>&times;</button>";
-		    html+="<h4 class='modal-title' id='modalTitle'><span class='glyphicon glyphicon-time' aria-hidden='true'></span> Please wait</h4>";
-		    html+="</div>";
-		    html+="<div class='modal-body' id='modalBody'>";
-                    html+="<p>Requesting this video...</p>";
-		    html+="</div>";
-		    html+="<div class='modal-footer'>";
-		    html+="<button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>";
-		    html+="</div>";
-	            html+="</div>";
-		    html+="</div>";
-		    html+="</div>";
-                    $('#search-result').append(html);
-                    $('.grid').masonry({
+                    $('#search-result').html(html);
+                    var $grid = $('.grid').masonry({
                         // options...
+                        initLayout: false,
                         itemSelector: '.grid-item',
                         columnWidth: 300,
                         fitWidth: true,
                         gutter: 10
                     });
-                    new Promise(function() {
-                        setTimeout(function() {
-                            window.dispatchEvent(new Event('resize'));
-                        }, 1500);
+                    $grid.masonry( 'on', 'layoutComplete', function() {
+                        console.log('layout is complete');
                     });
+                    // trigger initial layout
+                    $grid.masonry();
+
                 }
             });
         }
@@ -100,14 +86,14 @@ function requestVideo(id, duration){
         'durationString': duration
     };
     $.ajax({
-            url     : '../../server/scripts/api/queue.php',
-            data    : videoInfo,
-            dataType: 'json',
-            method  : 'post'
-        }).done(function(data){
-            console.log(data);
-        }).fail(function(data){
-            console.log(data);
-       });
+        url     : '../../server/scripts/api/queue.php',
+        data    : videoInfo,
+        dataType: 'json',
+        method  : 'post'
+    }).done(function(data){
+        console.log(data);
+    }).fail(function(data){
+        console.log(data);
+    });
 
 }
