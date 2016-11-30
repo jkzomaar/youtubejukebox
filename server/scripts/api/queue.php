d996ae7f600ff95dd4291d416df1d73415e3b1e4
Administrator <admin@example.com>
2016-11-30 17:08:52 +0100

    Added play next song, added comments to the regex, because no-one understands regex
    Signed-off-by: Administrator <admin@example.com>

--- server/scripts/api/queue.php
+++ server/scripts/api/queue.php
@@ -4,25 +4,52 @@ if(isset($_POST['addToQueue'])){
     echo json_encode(addToQueue($_POST['videoId'], $_POST['durationString']));
 }
 
+if(isset($_POST['nextSong'])){
+    echo json_encode(playNextSong($_POST['currentSongID']));
+}
+
 function addToQueue($videoId = false, $durationString = false){
     if(!$videoId || !$durationString){
         http_response_code(500);
         return 'All input fields are required';
     }
 
-    // if video id valid
+    // Allow only a-Z, 0-9, '_' and '-'
+    // Explanation: /i does toLower on the string
     if(preg_match('/[^a-z_\-0-9]/i', $videoId)){
         http_response_code(500);
         return 'Invalid videoID';
     }
 
-    // temp if $durationString is valid
+    // Allow only a-Z, 0-9 and ':'
+    // Explanation: /i does toLower on the string
     if(preg_match('/[^a-z:0-9]/i', $durationString)){
         http_response_code(500);
         return 'Invalid videoID';
     }
 
-    exec("bash -c 'exec nohup setsid bash /var/www/html/zomaar/server/scripts/download.sh $videoId $durationString  > /dev/null 2>&1 &'", $output, $returnValue);
+    exec("bash /srv/http/zomaarmusic.cinaed.be/public_html/server/scripts/download.sh $videoId $durationString", $output, $returnValue);
+    if($returnValue != '0'){
+        http_response_code(500);
+        return $output;
+    }
+    return $output;
+}
+
+function playNextSong($currentSongID = false){
+    if(!$currentSongID){
+        http_response_code(500);
+        return 'No song ID given';
+    }
+
+    // Allow only a-Z, 0-9, '_' and '-'
+    // Explanation: /i does toLower on the string
+    if(preg_match('/[^a-z_\-0-9]/i', $currentSongID)){
+        http_response_code(500);
+        return 'Invalid videoID';
+    }
+
+    exec("bash /srv/http/zomaarmusic.cinaed.be/public_html/server/scripts/nextsong.sh $currentSongID", $output, $returnValue);
     if($returnValue != '0'){
         http_response_code(500);
         return $output;
